1b7f35f46bd7e063f4c47e339db3d4e6ee79e0f2
gavimar <gavimar182@gmail.com>
2020-05-07 17:59:36 +0200

    final version

---
+++ js/script.js
@@ -0,0 +1,65 @@
+$(function() {
+      menu = $('nav ul');
+
+      $('#toggle-btn').on('click', function(e) {
+            e.preventDefault();
+            menu.slideToggle();
+      });
+
+      $(window).resize(function() {
+            var w = $(this).width();
+            if(w > 580 && menu.is(':hidden')) {
+                  menu.removeAttr('style');
+            }
+      });
+
+      $('nav li').on('click', function(e) {
+            var w = $(window).width();
+            if(w < 580) {
+                  menu.slideToggle();
+            }
+      });
+      
+      $( "#myForm" ).submit(function( event ) {
+            if($(".form-name").val() === "" || $(".form-email").val() === "" || $(".textarea").val() === "" ){
+                  alert( "Please fill out all the fields." );
+                  event.preventDefault();   
+                  
+            } else{
+            alert( "Thank you for your comment." );
+            event.preventDefault();
+            }
+          });
+
+});
+
+// smooth scrolling
+
+$('a[href*="#"]')
+.not('[href="#"]')
+.not('[href="#0"]')
+      .click(function(event) {
+            if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+      ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                  event.preventDefault();
+                  $('html, body').animate({
+                        scrollTop: target.offset().top
+                  }, 1000, function() {
+                        var $target = $(target);
+                        $target.focus();
+                        if($target.is(":focus")) {
+                              return false;
+                        } else {
+                              $target.attr('tabindex', '-1');
+                              $target.focus();
+                        };
+                  });
+            }
+      }
+      });
